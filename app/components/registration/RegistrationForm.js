e993baf2fc36151f811525d044cc2be6dbb1d560
Sergey Tarakanov <zolotara@gmail.com>
2018-08-01 14:18:22 +0300

    Styles for registration page

--- app/components/registration/RegistrationForm.js
+++ app/components/registration/RegistrationForm.js
@@ -9,24 +9,24 @@ class RegistrationForm extends Component {
   render() {
     const { handleSubmit, pristine, reset, submitting } = this.props;
     return (
-      <form onSubmit={handleSubmit}>
-        <Field name="name" type="text" component={inputField} placeholder="NAME *" validate={[
+      <form onSubmit={handleSubmit} className="registration-form">
+        <Field name="name" type="text" component={inputField} placeholder="Name*" validate={[
           required()
         ]} />
 
-        <Field name="email" type="email" component={inputField} placeholder="EMAIL*" validate={[
+        <Field name="email" type="email" component={inputField} placeholder="Email*" validate={[
           required()
         ]} />
 
-        <Field name="email_confirmation" type="email" component={inputField} placeholder="CONFIRM EMAIL*" validate={[
+      <Field name="email_confirmation" type="email" component={inputField} placeholder="Confirm Email*" validate={[
           required(),
           confirmation({ field: 'email', fieldLabel: 'Email' })
         ]} />
 
-        <Field name="organization" type="text" component={inputField} placeholder="ORGANIZATION" />
+        <Field name="organization" type="text" component={inputField} placeholder="Organization" />
         <Field name="tos" type="checkbox" component={checkboxField} label="I agree to the Terms and Conditions" validate={acceptance({ message: "Must accept Terms and Conditions" })} />
 
-        <button className='btn btn-default' type="submit">Create Wallet</button>
+        <button className='btn orange-button cta-button' type="submit">Create Wallet</button>
       </form>
     );
   }
