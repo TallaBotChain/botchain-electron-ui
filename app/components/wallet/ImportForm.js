946f7a5078e23e45b84b5026ac8f3574c5230832
Abliamit Abliamitov <ablyamyt@gmail.com>
2018-08-01 12:33:02 +0300

    use nav links instead of radio

--- app/components/wallet/ImportForm.js
+++ app/components/wallet/ImportForm.js
@@ -4,37 +4,47 @@ import {required,length,confirmation} from 'redux-form-validators'
 import {inputField, fileField, radioField} from '../form/FormFields'
 import { connect } from 'react-redux';
 import { formValueSelector } from 'redux-form';
-import { FormGroup, Col } from 'react-bootstrap';
+import { Nav, NavItem, Col } from 'react-bootstrap';
 
 class ImportForm extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { format: "private_key" };
+  }
+
+  changeFormat = (format) => {
+    this.setState({ format: format });
+  }
+
   render() {
     const {handleSubmit, pristine, reset, submitting, importFormat} = this.props;
     return (
       <form onSubmit={handleSubmit}>
         <Col md={3} sm={4} xs={5}>
-          <FormGroup>
-            <Field name="import_format" type="radio" component={radioField} value="private_key" label="Private Key" />
-            <Field name="import_format" type="radio" component={radioField} value="mnemonic" label="Backup Passphrase" />
-            <Field name="import_format" type="radio" component={radioField} value="json" label="JSON file" />
-          </FormGroup>
+          <Nav bsStyle="pills" stacked activeKey={this.state.format} onSelect={this.changeFormat}>
+            <NavItem eventKey={"private_key"} title="Private Key">Private Key</NavItem>
+            <NavItem eventKey={"mnemonic"} title="Backup Passphrase">Backup Passphrase</NavItem>
+            <NavItem eventKey={"json"} title="JSON file">JSON file</NavItem>
+          </Nav>
         </Col>
         <Col md={6} sm={8} xs={7}>
-          {(importFormat=="private_key") && <Field name="private_key" type="text" component={inputField} label="Private Key*" placeholder="" validate={[
+          {(this.state.format=="private_key") && <Field name="private_key" type="text" component={inputField} label="Private Key*" placeholder="" validate={[
             required()
             ]}/>
           }
 
-          {(importFormat=="mnemonic") && <Field name="mnemonic" type="text" component={inputField} label="Backup Passphrase*" placeholder="" validate={[
+          {(this.state.format=="mnemonic") && <Field name="mnemonic" type="text" component={inputField} label="Backup Passphrase*" placeholder="" validate={[
             required()
             ]}/>
           }
 
-          {(importFormat=="json") && <Field name="json" type="file" component={fileField} label="Choose File" placeholder="Upload JSON file" validate={[
+          {(this.state.format=="json") && <Field name="json" type="file" component={fileField} label="Choose File" placeholder="Upload JSON file" validate={[
             required()
             ]}/>
           }
 
-          {(importFormat) && (<div>
+          {(this.state.format) && (<div>
           <Field name="password" type="password" component={inputField} label="Password*" placeholder="Password*" validate={[
             required(),
             length({min: 8})
